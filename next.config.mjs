576bb5519a7a24cd61252daf8b45df052ece3af9
Casadenza <info@casadenza.com>
2026-03-07 17:54:14 +0530

    fix next-pwa config for vercel build

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,13 @@ import withPWA from "next-pwa";
 
 const isDev = process.env.NODE_ENV === "development";
 
+const nextConfig = {
+  reactStrictMode: true,
+};
+
 export default withPWA({
   dest: "public",
   disable: isDev,
   register: true,
   skipWaiting: true,
-})({
-  reactStrictMode: true,
-});
+})(nextConfig);
