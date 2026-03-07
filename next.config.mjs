4b18edee17cb25cf30663ae5a06d77117d3d1859
Casadenza <info@casadenza.com>
2026-03-07 18:05:47 +0530

    disable pwa for vercel build

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
-import withPWA from "next-pwa";
-
-const isDev = process.env.NODE_ENV === "development";
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-export default withPWA({
-  dest: "public",
-  disable: isDev,
-  register: true,
-  skipWaiting: true,
-})(nextConfig);
+export default nextConfig;
